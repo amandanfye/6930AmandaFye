db6d0f5e79369c73dd9be846edc8fcaa238801c3
amandanfye <59941106+amandanfye@users.noreply.github.com>
2020-04-15 18:09:28 -0400

    Add files via upload

--- EME6930/FinalProject/js/main.js
+++ EME6930/FinalProject/js/main.js
@@ -8,6 +8,18 @@ function hamburger() {
   }
 }
 
+//Accordian FAQ Menus
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
+
+
 //Contact Form
 (function ($) {
     "use strict";
